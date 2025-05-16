326a39843ba1116fc886b282de2cdd19d2346c9a
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-05-16 11:15:44 +0600

    OK

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -65,32 +65,31 @@ const Home = () => {
                   </Link>
                 </div>
 
-
-<div className="bg-white flex flex-col mt-20">
-  <div className="flex-grow"></div>
-  <div className="pb-20 px-6 sm:px-8 lg:px-12">
-    <div className="max-w-3xl mx-auto text-center">
-      <h1 className="text-5xl font-extrabold text-red-600 mb-6">
-        Join Us in Saving Lives
-      </h1>
-      <p className="text-xl text-gray-800 mb-5">
-        Your generous contribution empowers us to host more blood donation camps,
-        respond swiftly to emergencies, and support critical medical procedures.
-      </p>
-      <p className="text-lg text-gray-600 mb-8">
-        Every donation counts. With your help, we can make a greater impact
-        and bring hope to those in need.
-      </p>
-      <Link
-        to="/donate"
-        className="bg-red-600 text-white text-lg px-8 py-3 rounded-lg shadow-md hover:bg-red-700 transition duration-300 ease-in-out inline-block text-center"
-      >
-        Donate Money
-      </Link>
-    </div>
-  </div>
-</div>
-
+                <div className="bg-white flex flex-col mt-20">
+                  <div className="flex-grow"></div>
+                  <div className="pb-20 px-6 sm:px-8 lg:px-12">
+                    <div className="max-w-3xl mx-auto text-center">
+                      <h1 className="text-5xl font-extrabold text-red-600 mb-6">
+                        Join Us in Saving Lives
+                      </h1>
+                      <p className="text-xl text-gray-800 mb-5">
+                        Your generous contribution empowers us to host more blood donation
+                        camps, respond swiftly to emergencies, and support critical medical
+                        procedures.
+                      </p>
+                      <p className="text-lg text-gray-600 mb-8">
+                        Every donation counts. With your help, we can make a greater impact
+                        and bring hope to those in need.
+                      </p>
+                      <Link
+                        to="/donate"
+                        className="bg-red-600 text-white text-lg px-8 py-3 rounded-lg shadow-md hover:bg-red-700 transition duration-300 ease-in-out inline-block text-center"
+                      >
+                        Donate Money
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </>
             )}
           </div>
