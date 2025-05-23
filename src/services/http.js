da6cfc54e515a2c222453199a183a8798468efe2
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-05-23 23:00:54 +0600

    OK

--- src/services/http.js
+++ src/services/http.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", 
+  baseURL: "https://roktofy.vercel.app/api", 
 });
 
 apiClient.interceptors.request.use(
